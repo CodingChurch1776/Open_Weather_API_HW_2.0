ea296bff8f01e1e9c3466b3a24bef583d15efb83
Nicholas Walker <niwalker@westmont.edu>
2021-07-22 11:14:12 -0600

    second commit

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ console.log(form)
 form.addEventListener('submit', ( event ) => {
     event.preventDefault();
     let query_first = document.querySelector('#city')
-
     console.log(event)
     console.log(query_first)
 })
@@ -16,32 +15,46 @@ const getData = async () => {
     return response.data
 }
 
-// const DOM_ELEMENTS = {
-//     weather_data:'#weather-data'
-// }
-//     high_temp: '.high_temp',
-//     low_temp: '.low_temp',
-//     forecast: '.forecast',
-//     humidity: '.humidity'
-// }
+const DOM_ELEMENTS = {
+    weather_data:'#data-table'
+}
+
+const create_weather_table = (temp_max,temp_min,forecast,humidity) => {
+    const html=`<a href = "#" class ="list-group-item list-group item" > ${temp_max}${temp_min}${forecast}${humidity}</a>`;
+    document.querySelector(DOM_ELEMENTS.data_table).insertAdjacentHTML('beforeend', html)
+}
+
+const load_data = async()=>{
+    const weather_report = await getData();
+    console.log(weather_report)
+    let high = weather_report.main.temp_max
+    document.querySelector('#high_temp').append(high + ' °F')
+    let low = weather_report.main.temp_min
+    document.querySelector('#low_temp').append(low + ' °F')
+    let humidity = weather_report.main.humidity
+    document.querySelector('#humidity').append(humidity + '%')
+    let forecast = weather_report.weather[0].description
+    document.querySelector('#forecast').append(forecast)
+    console.log(forecast)
+
+
+const clear_data = () => {
+    document.getElementsByTagName().reset();
+}
+
+
+    //weather_report.forEach( element => create_weather_table(element.temp_max, element.temp_min, element.humidity))
+    
+    // weather_points = weatherdata.main.temp_max
+    // console.log(weather_points)
+}
 
 //  creation of weather data html
-
-const create_weather_table = (temp_max,temp_min,pressure,humidity) => {
-    const weather_table=`
-        <tr class="weather_object">
+{/* <tr class="weather_object">
             <td>${temp_max}</td>
             <td>${temp_min}</td>
             <td>${pressure}</td>
             <td>${humidity}</td>
-        </tr>`;
-    document.querySelector(DOM_ELEMENTS.weather_report).insertAdjacentHTML('beforeend', weather_table)
-}
-const load_data = async()=>{
-    const weather_data = await getData();
-    let weather_object = weather_data.main.slice(2,6)
-    weather_data.forEach( element => create_weather_table(element.temp_min, element.temp_max, element.pressure, element.humidity))
-
-}
+</tr>`; */}
 
 
